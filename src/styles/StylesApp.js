66cf5d4786bc2867cd1290d69fac9d1d32b3c0db
Rodrigo Valle <fernandovalle939@gmail.com>
2022-10-11 18:45:00 -0600

    Creando estilos y renderizando componentes

---
+++ src/styles/StylesApp.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+const stylesApp = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00033f',
+  },
+   Subcontainer: {
+    marginVertical: 15,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor:'white',
+    borderRadius: 15,
+    padding: 10,
+  },
+  texto: {
+    color: 'white'
+  },
+  titulo: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  Imgs: {
+    width: 60,
+    height: 60,
+    borderRadius: 0,
+  },
+ 
+  ImageContainer: {
+    flex: 1,
+  },
+  TextContaner: {
+    flex: 4,
+    marginLeft: 30,
+  },
+});
+
+export default stylesApp;
