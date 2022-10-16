a58cba2bd5484a50c1a9def987e4c5f03b09dbc8
Cesar Ramírez <61605947+CesarRamirez778@users.noreply.github.com>
2022-10-16 13:57:35 -0600

    Nuevo update agregando botones
    funciones y diseño extra

--- components/Carrito.js
+++ components/Carrito.js
@@ -4,37 +4,52 @@ import { NavigationContainer } from '@react-navigation/native';
 import stylesApp from '../src/styles/StylesApp';
 import { Ionicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const DATA = [
   {
     id: '1',
     title: 'Caja de Tylenol',
-    cantidad: '2 unidades',
+    cantidad: '2 unidadades',
     oferta: 'Precio de oferta: $2.00',
     src:require('../src/img/img4.png'),
   },
   
   {
-    id: '5',
+    id: '2',
     title: 'Cerebrofos',
-    cantidad: '60 tabletas',
-    precio: '$6.00',
+    cantidad: '1 unidad',
+    oferta: '$3.00',
     src:require('../src/img/img5.jpg'),
   },
 
+  {
+    id: '3',
+    title: 'Cerebrofos',
+    cantidad: '1 unidad',
+    oferta: '$3.00',
+    src:require('../src/img/enjuague.jpeg'),
+  },
 
+{
+    id: '4',
+    title: 'Cerebrofos',
+    cantidad: '1 unidades',
+    oferta: '$3.00',
+    src:require('../src/img/protector.jpg'),
+  },
  
 ];
 
-const Item = ({ title, cantidad, precio,oferta, img }) => 
+  
+const Item = ({ title, cantidad, precio,oferta, img, }) => 
 ( 
   <View style={stylesApp.Subcontainer}>
-   
     <View style={stylesApp.ImageContainer}>    
       <Image style={stylesApp.Imgs} source={img} />
     </View>
    
-   
+    
     <View style={stylesApp.TextContaner}>
       <Text style={stylesApp.titulo}>{title}</Text>
       <Text>{cantidad}</Text>
@@ -46,25 +61,44 @@ const Item = ({ title, cantidad, precio,oferta, img }) =>
       <AntDesign name="pluscircle" size={24} color="black" /> 
       <Ionicons name="ios-trash-outline" size={30} color="blue" />
     </View>
-      
+
   </View>
  
 );
 
-function Promociones() {
+function Carrito() {
   const renderItem = ({ item }) => (
-    <Item title={item.title} cantidad={item.cantidad} precio={item.precio} oferta={item.oferta} img={item.src} />
+    <Item title={item.title} cantidad={item.cantidad} precio={item.precio} oferta={item.oferta} img={item.src} titleAPP={item.titleAPP}/>
   );
-
   return (
     <View style={stylesApp.container}>
+
+      <Text style={stylesApp.tituloCart}> Listado de compras 
+        <MaterialCommunityIcons name="cart-variant" size={24} color="white" /> 
+      </Text>
+       
       <FlatList
         data={DATA}
         renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
+        
+        keyExtractor={(item) => item.id} />
+
+      <Button title="Buscar Mas productos"/> 
+      
+      <View style={stylesApp.footerCART}>
+       
+        <Text style={stylesApp.totalPagoCart}>
+          TOTAL: $11 DOLARES 
+        </Text>
+
+        <Text style={stylesApp.continuarPago}>
+          CONTINUAR <AntDesign name="arrowright" size={15} color="white" />
+        </Text>
+      </View>
+      
     </View>
   );
 }
 
-export default Promociones;
+
+export default Carrito
