aad53ccd08ea1881b1395c0167b2efd7d63053ec
Cesar Ramírez <61605947+CesarRamirez778@users.noreply.github.com>
2022-11-07 22:26:50 -0600

    Update al descuento screen

--- VISTAS/screens/DescuentosScreen.js
+++ VISTAS/screens/DescuentosScreen.js
@@ -1,13 +1,64 @@
-import {Text,View} from 'native-base'
-import React from 'react'
+import {View, Flex,Pressable, ScrollView, Text ,Image} from 'native-base';
+import React from 'react';
+import Productos from '../../Data/Productos';
+import Colors from "../../Data/Colores"
+import {useNavigation} from "@react-navigation/native"            
 
 
-function Descuentos (){
-  return(
-    <View>
-      <Text>Descuentos</Text>
-    </View>
- )
+function Menu() {
+  
+  const navigation = useNavigation()
+ 
+
+  return (
+    
+    <ScrollView flex={1}>
+      <Flex
+        direction="flex"
+        px={7} 
+        pt={5}
+        >
+        {/* GENERA EL PRODUCTO AUTOMATICAMENTE */}
+        {Productos.map((product) =>(
+          <Pressable 
+            onPress={()=>navigation.navigate("DetalleProducto",product)}
+            key={product.id} 
+            w="100%" 
+            bg={Colors.blanco}
+            rounded="md" 
+            shadow={2} 
+            pt={1} 
+            my={3} 
+            pb={2}
+            overflow="hidden"
+            >
+            {/* Coloca una imagen al boton del producto */}
+            <Image
+            source={product.src}
+            alt={product.nombre}
+            marginTop="3"
+            marginLeft="3"
+            w="25%" h={20} 
+            resizeMode="contain" 
+            
+            />
+            {/* Coloca La informacion a los productos*/}
+            <Text fontWeight='bold' position="absolute" marginLeft="120" marginTop="4"paddingRight="2">
+              {product.nombre}
+            </Text>
+            <Text position="absolute" marginLeft="120" marginTop="9" >
+              10 unidades 
+            </Text>
+            <Text  position="absolute" marginLeft="120" marginTop="16">
+              {product.precio}
+            </Text>
+             
+          </Pressable>
+        ))}
+        
+      </Flex>
+    </ScrollView>
+  );
 }
 
-export default Descuentos;
+export default Menu;
